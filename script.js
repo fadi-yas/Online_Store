cbd4c712788d7452f70c47be1b3bbf35e7eca36c
fadi-yas <73274794+fadi-yas@users.noreply.github.com>
2023-06-19 09:56:04 +0300

    More tasks

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let cartQuantity = 0;
 
-
-
+score ={
+    wins:0,
+    loses:0,
+    ties:0
+}
+score = JSON.parse(localStorage.getItem('score')) || {
+    wins :0,
+    loses:0,
+    ties:0
+};
 function ComputerMove(){
     randomNumber = Math.random();
     let computerMove = '';
@@ -29,19 +37,49 @@ function PlayGame(PlayerMove){
         else{
             result = 'You Win.';
         }
-        alert(`The computer choose ${computerMove}, your choose is ${PlayerMove}, ${result}`);
+
+        if(result == 'You Win.'){
+            ++score.wins;
+        }
+        else if(result == 'You lose.'){
+            ++score.loses;
+        }
+        else if(result == 'tie.'){
+            ++score.ties;
+
+        }
+        localStorage.setItem('score', JSON.stringify(score));
+        document.querySelector('.result').innerHTML = result;
+        document.querySelector('.chooise').innerHTML = `You ${PlayerMove} - ${computerMove} Computer`;
+        resultElement();
+
+        // alert(`The computer choose ${computerMove}, your choose is ${PlayerMove}, ${result}\nWins = ${score.wins}, Loses = ${score.loses}, Ties = ${score.ties}`);
     }
     else if(PlayerMove === 'Paper'){
             if(computerMove === 'Rock'){
-                result = 'You win.';
+                result = 'You Win.';
             }
             else if(computerMove === 'Paper'){
-                result = 'tie';
+                result = 'tie.';
             }
             else{
                 result = 'You lose.';
             }
-            alert(`The computer choose ${computerMove}, your choose is ${PlayerMove}, ${result}`);
+            if(result == 'You Win.'){
+                ++score.wins;
+            }
+            else if(result == 'You lose.'){
+                ++score.loses;
+            }
+            else if(result == 'tie.'){
+                ++score.ties;
+    
+            }
+            localStorage.setItem('score', JSON.stringify(score));
+            document.querySelector('.result').innerHTML = result;
+            document.querySelector('.chooise').innerHTML = `You ${PlayerMove} - ${computerMove} Computer`;
+            resultElement();
+            // alert(`The computer choose ${computerMove}, your choose is ${PlayerMove}, ${result}\nWins = ${score.wins}, Loses = ${score.loses}, Ties = ${score.ties}`);
     }
 
     else if(PlayerMove === 'Scissors'){
@@ -49,11 +87,38 @@ function PlayGame(PlayerMove){
                 result = 'You lose.';
             }
             else if(computerMove === 'Paper'){
-                result = 'You win.';
+                result = 'You Win.';
             }
             else{
                 result = 'tie.';
             }
-            alert(`The computer choose ${computerMove}, your choose is ${PlayerMove}, ${result}`);
+            if(result == 'You Win.'){
+                ++score.wins;
+            }
+            else if(result == 'You lose.'){
+                ++score.loses;
+            }
+            else if(result == 'tie.'){
+                ++score.ties;
+    
+            }
+            localStorage.setItem('score', JSON.stringify(score));
+            document.querySelector('.result').innerHTML = result;
+            document.querySelector('.chooise').innerHTML = `You ${PlayerMove} - ${computerMove} Computer`;  
+            resultElement();
+            // alert(`The computer choose ${computerMove}, your choose is ${PlayerMove}, ${result}\nWins = ${score.wins}, Loses = ${score.loses}, Ties = ${score.ties}`);
+    }
+}
+function resultElement(){
+    
+    document.querySelector('.game_score').innerHTML = `Wins = ${score.wins}, Loses = ${score.loses}, Ties = ${score.ties}`;
+}
+function subscribeChanel(){
+    const buttonElement = document.querySelector('#subscribe_btn');
+    if(buttonElement.innerText === 'Subscribe'){
+        buttonElement.innerText = 'Subscribed';
+    }
+    else{
+        buttonElement.innerText = 'Subscribe';
     }
 }
